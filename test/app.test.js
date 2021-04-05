5a3cb98ad9264b540aa58ed27753554da429fe3b
Santiago <santiagodavidlopezbenavides@gmail.com>
2021-04-05 09:18:26 -0500

    test: add signup test

--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,42 @@
-const app = require('../src/app')
-const supertest = require('supertest')
+const { api, sequelize } = require('./utils')
 
-const api = supertest(app)
+beforeAll(() => sequelize.sync({ force: true }).then())
+
+afterAll(() => sequelize.close())
 
 describe('GENERAL TEST', () => {
   test('GET "/"', async () => {
     const res = await api.get('/').expect(200).expect('Content-Type', /json/)
     expect(res.body).toEqual({})
+  })
+})
+
+describe('AUTH TEST', () => {
+  test('GET "api/auth/signup"', () => {
+    return new Promise((resolve, reject) => {
+      api
+        .get('/api/auth/signup')
+        .send({
+          email: 'test@test.com',
+          password: 'test',
+          username: 'test',
+        })
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(201)
+        .expect('Content-Type', /json/)
+        .then((res) => {
+          expect(res.body.user).toHaveProperty('createdAt')
+          expect(res.body.user).toHaveProperty('email')
+          expect(res.body.user).toHaveProperty('id')
+          expect(res.body.user).toHaveProperty('password')
+          expect(res.body.user).toHaveProperty('updatedAt')
+          expect(res.body.user).toHaveProperty('username')
+          return resolve()
+        })
+        .catch((err) => {
+          return reject(err)
+        })
+    })
   })
 })
